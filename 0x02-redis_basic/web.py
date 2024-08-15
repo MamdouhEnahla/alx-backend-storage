32d247ed7c2aa6137f70d4dec02923e6b841362c
MamdouhEnahla <mamdouh57118@gmail.com>
2024-08-15 05:58:04 +0300

    web-update...

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -5,29 +5,47 @@ Web Caching and Tracking
 import requests
 import redis
 from functools import wraps
+from typing import Callable
 
 store = redis.Redis()
 
-def count_url_access(method):
-    """counts how many times a URL is accessed."""
-    @wraps(method)
-    def wrapper(url):
-        cached_key = f"cached:{url}"
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode('utf-8')
 
-        count_key = f"count:{url}"
-        html = method(url)
+def cache(ttl: int = 10) -> Callable:
+    """
+    Cache decorator with configurable TTL.
+    """
+    def decorator(func: Callable) -> Callable:
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            cached_key = f"cached:{args[0]}"
+            cached_data = store.get(cached_key)
+            if cached_data:
+                return cached_data.decode("utf-8")
 
+            result = func(*args, **kwargs)
+            store.set(cached_key, result, ex=ttl)
+            return result
+        return wrapper
+    return decorator
+
+
+def count_access(func: Callable) -> Callable:
+    """
+    Decorator to count the number of accesses.
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        count_key = f"count:{args[0]}"
         store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
-        return html
+        return func(*args, **kwargs)
     return wrapper
 
-@count_url_access
+
+@cache()
+@count_access
 def get_page(url: str) -> str:
-    """Retrieves and returns the HTML content of a URL."""
-    response = requests.get(url)
-    return response.text
+    """
+    Fetches HTML content of a URL.
+    """
+    res = requests.get(url)
+    return res.text
